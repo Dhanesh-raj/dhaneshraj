f627d9693582c20648e0582d6c9d69c1d08c8561
Dhanesh Raj <imdhaneshraj@gmail.com>
2023-03-15 16:08:37 +0530

    script.js calendar updated

--- script.js
+++ script.js
@@ -71,44 +71,78 @@ window.addEventListener('scroll', function() {
 });
 
 
-/* DHANESH RAJ*/
+/* DHANESH RAJ CALENDAR */
 
-// Select the body element
-const body = document.querySelector('body');
+const currentTime = document.querySelector("h1"),
+content = document.querySelector(".content"),
+selectMenu = document.querySelectorAll("select"),
+setAlarmBtn = document.querySelector("button");
 
-// Define an array of colors
-const colors = [
-  "#FF6B6B",
-  "#FFE66D",
-  "#3FBF3F",
-  "#1E90FF",
-  "#C0448F",
-  "#FF8C00",
-  "#4B0082",
-];
+let alarmTime, isAlarmSet,
+ringtone = new Audio("./files/ringtone.mp3");
 
-// Function to generate a random gradient background
-function generateGradient() {
-  // Generate a random index to select two colors from the array
-  const randomIndex1 = Math.floor(Math.random() * colors.length);
-  const randomIndex2 = Math.floor(Math.random() * colors.length);
-
-  // Select the two colors from the array using the random indexes
-  const color1 = colors[randomIndex1];
-  const color2 = colors[randomIndex2];
-
-  // Create the gradient background using the selected colors
-  const gradient = `linear-gradient(45deg, ${color1}, ${color2})`;
-
-  // Set the background of the body element to the gradient
-  body.style.background = gradient;
+for (let i = 12; i > 0; i--) {
+    i = i < 10 ? `0${i}` : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-// Call the generateGradient function on page load
-generateGradient();
+for (let i = 59; i >= 0; i--) {
+    i = i < 10 ? `0${i}` : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
+}
 
-// Call the generateGradient function whenever the user scrolls
-window.addEventListener('scroll', generateGradient);
+for (let i = 2; i > 0; i--) {
+    let ampm = i == 1 ? "AM" : "PM";
+    let option = `<option value="${ampm}">${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+setInterval(() => {
+    let date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    ampm = "AM";
+    if(h >= 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+    if (alarmTime === `${h}:${m} ${ampm}`) {
+        ringtone.play();
+        ringtone.loop = true;
+    }
+});
+
+function setAlarm() {
+    if (isAlarmSet) {
+        alarmTime = "";
+        ringtone.pause();
+        content.classList.remove("disable");
+        setAlarmBtn.innerText = "Set Alarm";
+        return isAlarmSet = false;
+    }
+
+    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+    if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
+        return alert("Please, select a valid time to set Alarm!");
+    }
+    alarmTime = time;
+    isAlarmSet = true;
+    content.classList.add("disable");
+    setAlarmBtn.innerText = "Clear Alarm";
+}
+
+setAlarmBtn.addEventListener("click", setAlarm);
+
+/*===========================CALENDAR*/
 
 
 
